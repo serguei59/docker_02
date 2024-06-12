dc6ca6cc919bf92c115258129a6683255b62d3a8
Serge Buasa <sergebuasa@gmail.com>
2024-06-13 01:05:53 +0200

    maj

---
+++ courses_v2_app/main_v2.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, HTTPException
+import uvicorn
+
+
+courses_v2_app = FastAPI()
+
+mon_dictionnaire_de_courses = {
+    "farine":[200,"grammes"], 
+    "oeuf":[6,"unite"]
+}
+
+@courses_v2_app.get("/")
+def index():
+    return {"bonjour, bienvenu sur l' API liste de courses"}
+
+@courses_v2_app.get("/get_dictionnaire")
+def get_dictionnaire():
+    if len(mon_dictionnaire_de_courses) == 0:
+        return {"le dictionnaire est vide"}
+    else:
+        return {"content": mon_dictionnaire_de_courses}
+    
+
